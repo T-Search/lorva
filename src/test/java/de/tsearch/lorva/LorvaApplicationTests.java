360545bfdf2a0f094fa4712a17f067a877a80597
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2022-04-14 02:17:20 +0200

    Add test config

--- src/test/java/de/tsearch/lorva/LorvaApplicationTests.java
+++ src/test/java/de/tsearch/lorva/LorvaApplicationTests.java
@@ -2,8 +2,10 @@ package de.tsearch.lorva;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LorvaApplicationTests {
 
     @Test
